fcc967d5feba97dc7803cb250bb1f5a459f8462b
Tokhir Asadov <guvalakat1603@gmail.com>
2025-11-06 09:41:10 +0500

    Add files via upload

---
+++ tech/index.js
@@ -0,0 +1,29 @@
+import javascript from "./javascript.png";
+import typescript from "./typescript.png";
+import html from "./html.png";
+import css from "./css.png";
+import reactjs from "./reactjs.png";
+import redux from "./redux.png";
+import tailwind from "./tailwind.png";
+import nodejs from "./nodejs.png";
+import mongodb from "./mongodb.png";
+import git from "./git.png";
+import figma from "./figma.png";
+import docker from "./docker.png";
+import threejs from "./threejs.svg";
+
+export {
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    threejs,
+};
